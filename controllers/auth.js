bb33c36b2540205b019e36da361effee97490c17
Nahuel Garcia <nhlgrc@hotmail.com>
2024-07-25 16:30:36 -0300

    31312

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,82 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User-model');
 
 const createUser = async ( req, res = response ) => {
 
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
+
+    try {
+
+        let user = await User.findOne({ email });
+
+        if( user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ya existe un usuario con ese email'
+            });
+        }
+
+        user = new User( req.body );
     
-    const user = new User( req.body );
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt) ;
 
-    await user.save();
-
-    res.status(201).json({
-        ok: true,
-        msg: "createUser"
-    })
+        await user.save();
+    
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al crear usuario.'
+        });
+    }
 }
 
-const loginUser = ( req, res = response ) => {
+const loginUser = async ( req, res = response ) => {
     
     const { email, password } = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msg: "loginUser"
-    })
+    try {
+
+        let user = await User.findOne({ email });
+
+        if( !user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'No son válidas las credenciales'
+            });
+        }
+
+        if ( !bcrypt.compareSync( password, user.password )) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'No son válidas las credenciales'
+            });
+        }
+
+        //Generar JWT
+
+
+        res.status(200).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'No son válidas las credenciales.'
+        });
+    }
+   
 }
 
 const revalidateToken = ( req, res = response ) => {
